53245fac2c71df3d6d6015fa3e5ecca6e141603a
Mike <mr.anderson.1725@gmail.com>
2013-12-11 23:57:46 -0600

    util in progress

---
+++ src/couchutil/Defaults.py
@@ -0,0 +1,71 @@
+import re, sys
+
+class InvalidDefaultsException(Exception):
+    def __init__(self, message):
+        super(InvalidDefaultsException, self).__init__(message)
+        self.message = message
+
+class Defaults:
+    
+    def __init__(self, defaultsFile=None):
+        self._init_parsers()
+        if defaultsFile != None:
+            self.process(defaultsFile)        
+    
+    def databaseURL(self, overrides={}):
+        return "%(protocol)s://%(userpart)s%(url)s/%(database)s"%\
+            self._merge_args(self.__defaultProperties, overrides)
+    
+    def process(self, defaultsFile):        
+        with open(defaultsFile, 'r') as f:
+            for line in f:                
+                if self.__whitespace.match(line) or\
+                     self.__comment.match(line):
+                    continue
+                processed = False
+                for attr, parser in self.__propertyParsers.iteritems():
+                    m = parser.match(line) 
+                    if m:
+                        self.__defaultProperties[attr] = m.group(1)
+                        processed = True
+                        break
+                if not processed:
+                    raise InvalidDefaultsException("invalid config line in %s: %s"%\
+                                                       (defaultsFile, line))
+                    
+    def _init_parsers(self):
+        for k in self.__defaultProperties:
+            self.__propertyParsers[k] = self._make_parser(k)
+    
+    def _make_parser(self, propertyName):
+        return re.compile(r"\s*" + propertyName + "\s*=\s*(.*?)\s*$")
+    
+    def _merge_args(self, base, override):
+        args = dict(base.items() + override.items())
+        userpart = args['user'] if args['user'].strip() != '' else ''
+        userpart+= ":%s"%args['pass'] if args['pass'].strip() != '' and\
+            userpart != '' else ''
+        args['userpart'] = "%s@"%userpart if userpart != '' else userpart
+        return args
+                        
+    __defaultDatabaseURL = ''
+    __defaultProperties = {'user':'', 'pass':'', 'protocol':'http', 'url':'localhost:5984', 'database':''}
+    __propertyParsers = {}
+    __comment = re.compile(r"^\s*\#.*$")
+    __whitespace = re.compile(r"^\s*$")
+
+###############
+## TEST CASE ##
+###############
+def doTests():
+    print 'tests started...'
+    try:
+        defaults = Defaults()
+        url = defaults.databaseURL()
+        
+        print 'tests passed'
+    finally:
+        print 'done' 
+
+if __name__ == "__main__":
+    doTests()
--- src/couchutil_doc.py
+++ src/couchutil_doc.py
@@ -1,3 +1,40 @@
-from couchutil.lib import test
+#!/usr/bin/python
+import sys, os, re
+from couchutil import Defaults
+from os.path import expanduser
 
-test.test()
+COMMAND_SYNTAX = "[design|docs]/<your_database>/<your_document>.json"
+GLOBAL_DEFAULTS_FILE = '%s/.couchutil'%expanduser("~")
+LOCAL_DEFAULTS_FILE = '%s/'%os.getcwd()
+
+def get_defaults():
+    defaults = None
+    nextDefaultsFile = GLOBAL_DEFAULTS_FILE
+    try:
+        #read global defaults, then the local
+        defaults = Defaults(nextDefaultsFile)
+        nextDefaultsFile = LOCAL_DEFAULTS_FILE
+        defaults.process(nextDefaultsFile)
+    except IOError as ex:
+        print "Unable to read defaults '%s' : "%(nextDefaultsFile, ex.message)
+        sys.exit(1)
+    except Defaults.InvalidDefaultsException as ex:
+        print "Error in defaults '%s': %s"%(nextDefaultsFile, ex.message)
+        sys.exit(1)
+    except:
+        print 'Error: %s' % sys.exc_info()[0]
+        sys.exit(1)
+    return defaults
+
+def main():
+    if len(sys.argv) != 2:
+        print 'Argument error, expecting: %s' % COMMAND_SYNTAX
+        sys.exit(1)    
+    
+    defaults = get_defaults()
+    target = sys.argv[1]
+
+    #TODO: os.path.walk
+    
+if __name__ == '__main__':
+    main()
