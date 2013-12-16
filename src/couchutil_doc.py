3182db93384734d4a220b41f3528757d20796047
Mike <mr.anderson.1725@gmail.com>
2013-12-15 19:18:44 -0600

    util

--- src/couchutil_doc.py
+++ src/couchutil_doc.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python
 import sys, os, re
-from couchutil import Defaults
 from os.path import expanduser
+from couchutil import Defaults, DesignDocumentPreprocessor, DocumentUpload
+from couchutil.Utilities import props
 
 COMMAND_SYNTAX = "[design|docs]/<your_database>/<your_document>.json"
 GLOBAL_DEFAULTS_FILE = '%s/.couchutil'%expanduser("~")
 LOCAL_DEFAULTS_FILE = '%s/'%os.getcwd()
+DIRECTORY_TYPE_MAP = {'design': DocumentUpload.DocumentUploadType.DESIGN,
+                      'docs': DocumentUpload.DocumentUploadType.DOCUMENT,}
+
+class _Parameters():
+    document_type = None
+    database_name = None
+    document_name = None
 
 def get_defaults():
     defaults = None
@@ -26,15 +34,47 @@ def get_defaults():
         sys.exit(1)
     return defaults
 
+def parse_parameters(targetStr):
+    targetParts = targetStr.split('/')
+    params = _Parameters()
+    #skip until a valid document type is hit, then assume a db name and
+    #document name follow immediately after
+    for pt in targetParts:
+        if not params.document_type:            
+            params.document_type = DIRECTORY_TYPE_MAP.get(pt, None)
+        elif not params.database_name:
+            params.database_name = pt
+        elif not params.document_name:
+            params.document_name = pt
+    for propName, val in props(params):
+        if not val:
+            raise Exception('Failed to parse parameter from argument: %s' % propName)
+
+def build_design_doc_url():
+    pass
+
 def main():
     if len(sys.argv) != 2:
         print 'Argument error, expecting: %s' % COMMAND_SYNTAX
-        sys.exit(1)    
-    
+        sys.exit(1)            
     defaults = get_defaults()
     target = sys.argv[1]
-
-    #TODO: os.path.walk
+    path = os.getcwd() + target
+    if not os.path.isfile(path):
+        print 'Argument error, "%s" is not a file' % (path)
+        sys.exit(1)
+    parameters = parse_parameters(target)
+    doc_url = '/'.join[defaults.dbmsURL + '/' + parameters.database_name,
+    if parameters.document_type == DocumentUpload.DocumentUploaparameters.document_type, document)
+        
+        documentUploader.upload()
+    else:
+        print 'Operation not implemented'
+        sys.exit(1)
+    
+    print 'upload done'
+    sys.exit(0)           
     
 if __name__ == '__main__':
     main()
+    
