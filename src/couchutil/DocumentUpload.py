67e45eabd092bd96ce893f8f05139ed5879b411a
Mike <mr.anderson.1725@gmail.com>
2013-12-21 02:34:11 -0600

    still working...

--- src/couchutil/DocumentUpload.py
+++ src/couchutil/DocumentUpload.py
@@ -1,35 +1,66 @@
 from Utilities import make_enum
 import sys, requests, json
 
-DocumentUploadType = make_enum(STANDARD=1, FORCE_UPDATE=2)
+ConflictResolverType = make_enum(FAIL=1, UPDATE_MERGE=2, UPDATE_OVERWRITE=3)
 
 COUCH_HEADER = {'Content-type':'application/json'}
+MAX_RESOLVE_TRIES = 10
 class DocumentUpload(object):
-    def __init__(self, databaseURL, documentUploadType=DocumentUploadType.STANDARD,
+    def __init__(self, conflictResolver=ConflictResolverType.FAIL,
                  verbose=False):
-        self._uploadType = documentUploadType
-        self._databaseURL = databaseURL
+        self._resolveType = conflictResolver
+        self._resolveCount = 0
         self._verbose = verbose
+        self._is_disposed = False
     
-    def upload(self, documentPath, document, isRetry=False):
-        url = '/'.join[self._databaseURL, documentPath]
-        self._log('Uploading to %s...' % url)        
+    def upload(self, url, document):
+        if self._is_disposed:            
+            raise Exception("Document already complete")                
+        while not self._is_disposed:                
+            try:
+                self._log('Uploading to %s...' % url)
+                resp = requests.post(url, data=document, headers=COUCH_HEADER)
+            except requests.exceptions.ConnectionError as ex:
+                self._log("Server error: %s" % (ex))
+                raise ex                                         
+            if (resp.status_code == 409 and
+                self._uploadType != ConflictResolverType.FAIL and
+                self._resolveCount < MAX_RESOLVE_TRIES):            
+                self._log("Resolving document conflict...")
+                self._resolveCount += 1
+                self._resolveConflict(url, document)                        
+            elif resp.status_code != 201:
+                msg = "HTTP Request error: %s %s" % (resp.status_code, resp.text)
+                self._log(msg)
+                self._is_disposed = True
+                raise requests.exceptions.HTTPError(msg)
+            else:
+                self._is_disposed = True
+    
+    def _resolveConflict(self, url, document):        
         try:
-            resp = requests.post(url, data=document, headers=COUCH_HEADER)
-        except requests.exceptions.ConnectionError as ex:
-            self._log("Server error: %s" % (ex))
-            raise ex                         
-        if (not isRetry and resp.status_code == 409 and 
-            self._uploadType == DocumentUploadType.FORCE_UPDATE):
-            self._log("Document conflict retrying...")
-            self._upload(documentPath, self._updateDoc(documentPath, document),
-                         False)
-        elif resp.status_code != 201:
-            msg = "HTTP Request error: %s %s" % (resp.status_code, resp.text)
-            self._log(msg)
-            raise requests.exceptions.HTTPError(msg)
+            current_document = json.loads(requests.get(url))
+        except Exception as ex:
+            pass         
+        if self._resolveType == ConflictResolverType.UPDATE_MERGE:
+            #merge properties not present in document
+            for k,v in current_document.iteritems():
+                document = document.get(k, v)
+            
+        elif self._resolveType == ConflictResolverType.UPDATE_OVERWRITE:
+            document["id"] = current_document["id"]
     
-    def _uploadDoc(self, documentPath, document):
+    
+    def _recursive_merge(self, merge_from, merge_into):
+        for k,v in merge_from.iteritems():
+            if isinstance(v, dict):
+                merge_into[k] = merge_into.get(k, dict())
+                if isinstance(merge_into[k], dict):
+                    self._recursive_merge(v, merge_into[k])
+            else:
+                merge_into[k] = merge_into.get(k, v)
+    
+    def _upload(self, documentPath, document):
         pass
     
     def _log(self, str):
@@ -38,4 +69,5 @@ class DocumentUpload(object):
     
     _verbose = False
     _databaseURL = None
-    _uploadType = None
+    _resolveType = None
+    _resolveCount = 0
--- src/couchutil_doc.py
+++ src/couchutil_doc.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python
 import sys, os, re
 from os.path import expanduser
-from couchutil import Defaults, DesignDocumentPreprocessor, DocumentUpload
-from couchutil.Utilities import props
+from couchutil import Defaults, DocumentPreprocessor
+from couchutil.DocumentUpload import DocumentUpload, ConflictResolverType
+from couchutil.Utilities import props, make_enum
+
+DocumentType = make_enum(DOCUMENT=1, DESIGN=2)
 
 COMMAND_SYNTAX = "[design|docs]/<your_database>/<your_document>.json"
 GLOBAL_DEFAULTS_FILE = '%s/.couchutil'%expanduser("~")
 LOCAL_DEFAULTS_FILE = '%s/'%os.getcwd()
-DIRECTORY_TYPE_MAP = {'design': DocumentUpload.DocumentUploadType.DESIGN,
-                      'docs': DocumentUpload.DocumentUploadType.DOCUMENT,}
-
+DIRECTORY_TYPE_MAP = {'design': DocumentType.DESIGN,
+                      'docs': DocumentType.DOCUMENT}
 class _Parameters():
     document_type = None
     database_name = None
@@ -50,8 +52,13 @@ def parse_parameters(targetStr):
         if not val:
             raise Exception('Failed to parse parameter from argument: %s' % propName)
 
-def build_design_doc_url():
-    pass
+def build_doc_url(defaults, parameters):    
+    db_url = '/'.join[defaults.dbmsURL(), parameters.database_name]
+    if parameters.document_type == DocumentType.DESIGN:
+        db_url += '/_design/%s'%parameters.document_name
+    elif parameters.document_type == DocumentType.DOCUMENT:
+        db_url += '/%s'%parameters.document_name
+    return db_url    
 
 def main():
     if len(sys.argv) != 2:
@@ -64,13 +71,24 @@ def main():
         print 'Argument error, "%s" is not a file' % (path)
         sys.exit(1)
     parameters = parse_parameters(target)
-    doc_url = '/'.join[defaults.dbmsURL + '/' + parameters.database_name,
-    if parameters.document_type == DocumentUpload.DocumentUploaparameters.document_type, document)
-        
-        documentUploader.upload()
-    else:
-        print 'Operation not implemented'
-        sys.exit(1)
+    doc = DocumentPreprocessor(path).get_document()
+    url = build_doc_url(defaults, parameters)    
+    up = DocumentUpload(ConflictResolverType.UPDATE_MERGE)
+    try:
+        up.upload(url, doc)
+    except Exception as ex:
+        pass
+            
+    #doc_up = DocumentUpload.DocumentUpload(db_url,
+    #                                      DocumentUploadType.FORCE_UPDATE_OVERWRITE,
+    #                                       True)
+    #doc_up.upload(parameters.document_name,
+    #              document, isRetry)
+    #if parameters.document_type == DocumentUpload.DocumentUploaparameters.document_type, document):        
+    #    documentUploader.upload()
+    #else:
+    #    print 'Operation not implemented'
+    #    sys.exit(1)
     
     print 'upload done'
     sys.exit(0)           
